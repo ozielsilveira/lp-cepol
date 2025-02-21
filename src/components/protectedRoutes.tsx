791b0580699f58a9267d0ddcfc3d0f704da6c5dc
ozielsilveira <11690827-silveiraoziel@users.noreply.gitlab.com>
2025-02-20 22:29:59 -0300

    fix: validar token de autenticação em rotas protegidas

--- src/components/protectedRoutes.tsx
+++ src/components/protectedRoutes.tsx
@@ -5,13 +5,34 @@ interface ProtectedRouteProps {
     children: React.ReactElement;
 }
 
-const ProtectedRoute: React.FC<ProtectedRouteProps> = ({children}) => {
-    const isAuthenticated = !!localStorage.getItem("authToken");
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+    const token = localStorage.getItem("authToken");
 
-    if (!isAuthenticated) {
+    if (!token) {
         return <Navigate to="/auth" replace />;
     } else {
-        return children;
+        if (IsValidToken(token)) {
+            return children;
+        } else {
+            localStorage.removeItem("authToken");
+            return <Navigate to="/auth" replace />;
+        }
+    }
+};
+
+const IsValidToken = (token: string): boolean => {
+    try {
+        const payload = token.split('.')[1];
+        const decodedPayload = atob(payload);
+        const jsonPayload = JSON.parse(decodedPayload);
+
+        const expiration = jsonPayload.exp;
+        const now = Math.floor(Date.now() / 1000);
+
+        return now < expiration;
+    } catch (error) {
+        console.error('Erro ao verificar o token:', error);
+        return false;
     }
 };
 
